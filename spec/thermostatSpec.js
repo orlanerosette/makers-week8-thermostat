ad35993c1cb2e8feaa1da93525efae54bf4aa6ca
Orlane <orlanedoumbe@gmail.com>
2022-04-13 13:22:49 +0100

    Completed tests and refactored class

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,47 +1,183 @@
-describe('Thermostat', function() {
+'use strict';
+
+describe('Thermostat', () => {
   let thermostat;
-  beforeEach(function() {
+  beforeEach(() => {
     thermostat = new Thermostat();
   });
 
-  it('Thermostat starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+  describe('Default mode', () => {
+    
+    it('Thermostat starts at 20 degrees', () => {
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+
+    it('Thermostat has a minimum of 10 degrees', () => {
+      for (let i = 0; i < 11; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(10);
+    });
+  
+    it('Power saving mode is on', () => {
+      expect(thermostat.isPowerSavingModeOn()).toBe(true);
+    });
+
+    it('Enery Usage is set to "medium-usage"', () => {
+      expect(thermostat.energyUsage()).toEqual("medium-usage");
+    });
+
+  });
+  
+
+  describe('You can increase the temperature with an up function', () => {
+
+    it('Thermostat can increase by 1 degree', () => {
+      thermostat.up()
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
+    });
+
+    it('Thermostat can increase by 5 degrees', () => {
+      for (let i = 0; i < 5; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+
+    it('Thermostat can increase by 10 degrees', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 10; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(30);
+    });
+
   });
 
-  describe('You can increase the temperature with an up function', function() {
+  describe('You can decrease the temperature with a down function', () => {
 
-    it('Thermostat can increase by 1 degree', function() {
-      thermostat.up(1)
-      expect(thermostat.temperature).toEqual(21);
+    it('Thermostat can decrease by 1 degree', () => {
+      thermostat.down();
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
     });
 
-    it('Thermostat can increase by 5 degrees', function() {
-      thermostat.up(5)
-      expect(thermostat.temperature).toEqual(25);
+    it('Thermostat can decrease by 5 degrees', () => {
+      for (let i = 0; i < 5; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(15);
     });
 
-    it('Thermostat can increase by 10 degrees', function() {
-      thermostat.up(10)
-      expect(thermostat.temperature).toEqual(30);
+    it('Thermostat can decrease by 10 degrees', () => {
+      for (let i = 0; i < 10; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(10);
     });
 
   });
 
-  describe('You can decrease the temperature with a down function', function() {
+  describe('Power saving mode is on', () => {
 
-    it('Thermostat can decrease by 1 degree', function() {
-      thermostat.down(1)
-      expect(thermostat.temperature).toEqual(19);
+    it('Maximum temperature is 25 degrees', () => {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
     });
 
-    it('Thermostat can decrease by 5 degrees', function() {
-      thermostat.down(5)
-      expect(thermostat.temperature).toEqual(15);
+    it('can turn Power saving mode back on', () => {
+      thermostat.switchPowerSavingModeOff();
+      expect(thermostat.isPowerSavingModeOn()).toBe(false);
+      thermostat.switchPowerSavingModeOn();
+      expect(thermostat.isPowerSavingModeOn()).toBe(true);
     });
 
-    it('Thermostat can decrease by 10 degrees', function() {
-      thermostat.down(10)
-      expect(thermostat.temperature).toEqual(10);
+  });
+
+  describe('Power saving mode is off', () => {
+
+    it('Maximum temperature is 32 degrees', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 13; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+
+  });
+
+  describe('Reset function:', () => {
+
+    it('Can reset the temperature to 20 degrees, the default temperature', () => {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      };
+      thermostat.resetTemp();
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+
+  });
+
+  describe('Energy usage levels:', () => {
+
+    it('when the thermostat is 10 degrees, "low-usage"', () => {
+      for (let i = 0; i < 11; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.energyUsage()).toEqual("low-usage");
+    });
+
+    it('when the thermostat is 15 degrees, "low-usage"', () => {
+      for (let i = 0; i < 5; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.energyUsage()).toEqual("low-usage");
+    });
+
+    it('when the thermostat is 17 degrees, "low-usage"', () => {
+      for (let i = 0; i < 3; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.energyUsage()).toEqual("low-usage");
+    });
+
+    it('when the thermostat is 18 degrees, "medium-usage"', () => {
+      for (let i = 0; i < 2; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.energyUsage()).toEqual("medium-usage");
+    });
+
+    it('when the thermostat is 25 degrees, "medium-usage"', () => {
+      for (let i = 0; i < 5; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.energyUsage()).toEqual("medium-usage");
+    });
+
+    it('when the thermostat is 26 degrees, "high-usage"', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.energyUsage()).toEqual("high-usage");
+    });
+
+    it('when the thermostat is 30 degrees, "high-usage"', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 10; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.energyUsage()).toEqual("high-usage");
+    });
+
+    it('when the thermostat is 32 degrees, "high-usage"', () => {
+      thermostat.switchPowerSavingModeOff();
+      for (let i = 0; i < 12; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.energyUsage()).toEqual("high-usage");
     });
 
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,65 @@
+'use strict';
+
 class Thermostat {
   constructor() {
-    this.temperature = 20;
+    this.DEFAULT_TEMP = 20;
+    this.temperature = this.DEFAULT_TEMP;
+    this.MED_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+    this.powerSavingMode = true;
+    this.MIN_TEMP = 10;
+    this.MAX_TEMP_PSM_ON = 25;
+    this.MAX_TEMP_PSM_OFF = 32;
+    this.DEFAULT_TEMP = 20;
   };
 
-  up(degree) {
-    this.temperature += degree;
+  getCurrentTemperature() {
+    return this.temperature;
   };
 
-  down(degree) {
-    this.temperature -= degree;
+  isMinTemp() {
+    return this.temperature === this.MIN_TEMP;
+  };
+
+  isMaxTemp() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_TEMP_PSM_OFF;
+    }
+    return this.temperature === this.MAX_TEMP_PSM_ON;
+  };
+
+  up() {
+    if (this.isMaxTemp()) {
+      return;
+    }
+    this.temperature += 1;
+  };
+
+  down() {
+    if(this.isMinTemp()) {
+      return;
+    } 
+    this.temperature -= 1;
+  };
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  };
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  };
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  };
+
+  resetTemp() {
+    this.temperature = this.DEFAULT_TEMP;
+  };
+
+  energyUsage() {
+    return this.temperature < this.MED_ENERGY_USAGE_LIMIT ? "low-usage" : this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT ? "medium-usage" : "high-usage";
   };
 
 };
